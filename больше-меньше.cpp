cc83e273e5c5c4601362da049860e3d60c132796
Rustem <48765033+RustemCoder@users.noreply.github.com>
2020-04-18 23:03:14 +0600

    Create больше-меньше.cpp

---
+++ больше-меньше.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+int main()
+{
+     
+int n,m;
+    cin>>n>>m;
+     
+    if(n>m)
+    cout<<">";
+    if(n<m)
+    cout<<"<";
+     
+    if(n==m)
+    cout<<"=";
+     
+    return 0;
+}
