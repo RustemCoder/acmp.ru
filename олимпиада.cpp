44430400ff234077f9086f9b63ddb72c29aa55e0
Rustem <48765033+RustemCoder@users.noreply.github.com>
2020-04-18 23:02:13 +0600

    Create олимпиада.cpp

---
+++ олимпиада.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+ 
+using namespace std;
+int main()
+{
+     
+     
+     
+     
+    int n;
+    cin>>n;
+    int a[n],i;
+     
+     
+    for(i=1;i<=n;i++)
+    {
+         
+         
+        cin>>a[i];
+         
+         
+         
+         
+         
+         
+    }
+    cout<<"1";
+    return 0;
+}
