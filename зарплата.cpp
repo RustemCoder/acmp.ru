512637cb6737612e6cbc60b23ab66c0f153fd629
Rustem <48765033+RustemCoder@users.noreply.github.com>
2020-04-18 22:55:15 +0600

    Create зарплата.cpp

---
+++ зарплата.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+int main()
+{
+     int a,b,c;
+     
+    cin>>a>>b>>c;
+     
+        if(a>=b && b>=c)
+    cout<<a-c;
+        else if(a>=c && c>=b)
+    cout<<a-b;
+     
+    else if(b>=c &&  c>=a)
+    cout<<b-a;
+     
+    else if(c>=a  && a>=b)
+    cout<<c-b;
+     else if(c>=b && b>=a)
+    cout<<c-a;
+    else if(b>=a && a>=c)
+    cout<<b-c;
+ 
+    return 0;
+ 
+     
+}
