931d878a19582831ca9055b90982b744999af1ca
Rustem <48765033+RustemCoder@users.noreply.github.com>
2020-04-20 16:46:10 +0600

    Create торт.cpp

---
+++ торт.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>1){
+     
+    if(n%2==0)
+    cout<<n/2;
+    else if(n%2!=0)
+    cout<<n;
+}
+    else
+    cout<<"0";
+    return 0;
+     
+     
+}
