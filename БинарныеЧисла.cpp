5a55c64c75a07449ae92c72e1e83d826cc02bdc7
Rustem <48765033+RustemCoder@users.noreply.github.com>
2020-06-07 19:07:21 +0600

    Бинарные Числа

---
+++ БинарныеЧисла.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{int a;
+	cin>>a;
+if(a==1 || a==2 || a==4 || a==8 || a==16 || a==32 || a==64 ||a==128 || a== 256 || a==512 || a==1024 || a==2048 || a==4096 || a==8192 )
+	cout<<"YES";
+	else
+	cout<<"NO";
+	return 0 ;
+}
